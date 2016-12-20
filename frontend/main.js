e964de0a3d8af3849f63dbf35415e4584b03e68e
Vincent Khougaz <vincent@khougaz.com>
2016-12-20 14:52:33 -0800

    Circle-line intersection math, drag line

--- frontend/main.js
+++ frontend/main.js
@@ -9,13 +9,15 @@ var PULLEYDISTANCE = 500,
     PADDING = 20,
     PULLEYLINELENGTH = 10,
     PULLEYLINESTROKE = 2,
+    DEBUGLINESTROKE = 3,
     COLOR = {
         BG: '#eee',
         PLOTAREA: '#ddd',
         PULLEY: '#999',
         PULLEYLINE: '#000',
         PLOTTER: 'yellow',
-        BELT: '#000'
+        BELT: '#000',
+        DEBUGLINE: '#f00'
     };
 
 
@@ -29,6 +31,10 @@ function intersectCircle(R, d, r) {
     var x = (d*d - r*r + R*R)/(2*d);
     var a = (1/d)*Math.sqrt((-d + r - R) * (-d - r + R) * (-d + r + R) * (d + r + R));
     return [x, a/2];
+}
+
+function sgn(n) {
+    return x < 0 ? -1 : 1;
 }
 // END MATH
 
@@ -94,9 +100,14 @@ plotter.append('line')
     .attr('stroke-width', BELTSTROKE)
     .attr('stroke', COLOR.BELT);
 
+var dragLine = plotter.append('line')
+    .attr('class', 'dragline')
+    .attr('stroke-width', DEBUGLINESTROKE)
+    .attr('stroke', COLOR.DEBUGLINE)
+    .attr('opacity', 0);
+
 plotter.append('g')
     .attr('class', 'plotter-head')
-
     .append('circle')
     .attr('cx', 0)
     .attr('cy', 0)
@@ -121,6 +132,26 @@ function pulleyControls(pulley, conf) {
     // Start in a right triangle
     var length = Math.SQRT2/2 * PULLEYDISTANCE;
     dl.attr('r', length);
+
+    function intersect(r, x1a, y1a, x2a, y2a) {
+        // http://mathworld.wolfram.com/Circle-LineIntersection.html
+        var x1 = x1a - cx,
+            y1 = y1a - cy,
+            x2 = x2a - cx,
+            y2 = y2a - cy;
+
+        var dx = x2 - x1,
+            dy = y2 - y1,
+            dr = Math.sqrt(dx*dx + dy*dy),
+            D = x1*y2 - x2*y1,
+            dis = r*r * dr*dr - D*D;
+
+
+        var xi1 = (D*dy + sgn(dy) * dx * Math.sqrt(r*r * dr*dr - D*D)) / (dr*dr),
+            xi2 = (D*dy - sgn(dy) * dx * Math.sqrt(r*r * dr*dr - D*D)) / (dr*dr),
+            yi1 = (-D*dx + Math.abs(dy) * Math.sqrt(r*r * dr*dr - D*D)) / (dr*dr),
+            yi2 = (-D*dx + Math.abs(dy) * Math.sqrt(r*r * dr*dr - D*D)) / (dr*dr);
+    }
 
     return {
         rotate: function (deg) {
@@ -148,6 +179,8 @@ function plotterControls(plotter, pulleyC1, pulleyC2) {
     belt1.attr('x1', pulleyC1.x()).attr('y1', pulleyC1.y());
     belt2.attr('x1', pulleyC2.x()).attr('y1', pulleyC2.y());
 
+    var plotterX, plotterY;
+
     function pulleyIntersection() {
         var i = intersectCircle(pulleyC1.length(), pulleyC2.x() - pulleyC1.x(), pulleyC2.length());
         return [i[0] + pulleyC1.x(), i[1] + pulleyC1.y()];
@@ -155,16 +188,32 @@ function plotterControls(plotter, pulleyC1, pulleyC2) {
 
     return {
         update: function() {
-            var plotterPos = pulleyIntersection(),
-                plotterX = plotterPos[0],
-                plotterY = plotterPos[1];
+            var plotterPos = pulleyIntersection();
+            plotterX = plotterPos[0];
+            plotterY = plotterPos[1];
 
 
             belt1.attr('x2', plotterX).attr('y2', plotterY);
             belt2.attr('x2', plotterX).attr('y2', plotterY);
 
             plotterHead.attr('transform', 'translate(' + plotterX + ',' + plotterY + ')');
-        }
+        },
+        pos: function() { return [plotterX, plotterY]; }
+    }
+}
+
+function lineControl(line) {
+    return {
+      hide: function() {
+        line.attr('opacity', 0)
+      },
+      show: function(start, end) {
+        line.attr('opacity', 1)
+            .attr('x1', start[0])
+            .attr('y1', start[1])
+            .attr('x2', end[0])
+            .attr('y2', end[1]);
+      }
     }
 }
 
@@ -173,6 +222,8 @@ var lPulleyControls = pulleyControls(lPulley, {dir:  1}),
 
 var mainPlotterControls = plotterControls(plotter, lPulleyControls, rPulleyControls);
 
+var dragLineControls = lineControl(dragLine);
+
 var plotAreaX1 = parseFloat(plotArea.attr('x')),
     plotAreaX2 = parseFloat(plotArea.attr('width')) + plotAreaX1,
     plotAreaY1 = parseFloat(plotArea.attr('y')),
@@ -180,11 +231,6 @@ var plotAreaX1 = parseFloat(plotArea.attr('x')),
 
 var x = d3.scaleLinear().range([plotAreaX1, plotAreaX2]),
     y = d3.scaleLinear().range([plotAreaY1, plotAreaY2]);
-
-hover.on("mousemove", function () {
-    var mouseX = d3.mouse(this)[0],
-        mouseY = d3.mouse(this)[1];
-});
 // END ATTACH
 
 // START UI LOGIC
@@ -211,13 +257,48 @@ var Key = {
 
 window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
 window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
+
+var startDrag;
+var endDrag;
+hover.on('mousedown', function(event) {
+  startDrag = d3.mouse(this);
+  endDrag = undefined;
+  d3.event.stopPropagation();
+});
+
+hover.on('mousemove', function(event) {
+  if (startDrag == undefined || endDrag != undefined)
+    return;
+
+  dragLineControls.show(startDrag, d3.mouse(this));
+  d3.event.stopPropagation();
+});
+
+hover.on('mouseup', function(event) {
+  endDrag = d3.mouse(this);
+  dragLineControls.show(startDrag, endDrag);
+  d3.event.stopPropagation();
+});
 // END UI LOGIC
 
-// START CONTROLLER LOGIC
+// START MOTION PLANNER
+/*
+Have position
+Have direction
 
+Intersect 4 circles, each stepper each direction
+pick least positive
+determine delay
+sleep
+step
+
+ */
+// END MOTION PLANNER
+
+// START CONTROLLER LOGIC
 mainPlotterControls.update();
 function loop() {
-    var speed = 20;
+    var speed = 10;
 
     if (Key.isDown(Key.UP)) {
         lPulleyControls.rotate(-speed);
@@ -239,8 +320,7 @@ function loop() {
     mainPlotterControls.update();
 }
 
-setInterval(loop, 100);
-
+setInterval(loop, 50);
 // END CONTROLLER LOGIC
 
 // lPulleyControls.rotate(90);
